e44dcfcf99e5719a2a9e90ee24311ea9fc8459da
Bastian <bastian.p@outlook.com>
2023-03-11 12:50:26 -0300

    Updated basePath

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+
 import 'bastianparedes/styles/global.css';
 import 'bastianparedes/styles/normalize.css';
+import { basePath } from '../next.config';
 
 const _App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
-        <link href="/favicon.ico" rel="icon" />
+        <link href={String(basePath) + '/favicon.ico'} rel="icon" />
         <title>Bastián Paredes</title>
       </Head>
       <Component {...pageProps} />
